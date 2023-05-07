a3ba03d7ae9f3453c99bb8ab2b0bd1321db926a0
weigan <1831197727@qq.com>
2023-05-07 21:20:06 +0800

    修复release程序集丢失问题；修复无法点击停止按钮问题；

--- CCodeAI/ToolWindows/CCodeExplainWindow.cs
+++ CCodeAI/ToolWindows/CCodeExplainWindow.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.Imaging;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,7 +17,38 @@ namespace CCodeAI
 
         public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
             return Task.FromResult<FrameworkElement>(new CCodeExplainWindowControl());
+        }
+
+        private System.Reflection.Assembly CurrentDomain_AssemblyResolve(
+            object sender, 
+            ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name).Name + ".dll";
+            var extensionLocation = Path.GetDirectoryName(typeof(CCodeExplainWindow).Assembly.Location);
+
+            try
+            {
+
+                var file = Path.Combine(extensionLocation, assemblyName);
+
+                if (File.Exists(file))
+                {
+                    return Assembly.LoadFrom(file);
+                }
+                else
+                {
+                    Debug.Print(file);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(
+                    string.Format("The location of the assembly, {0} could not be resolved for loading.",
+                    extensionLocation), ex);
+            }
         }
 
         [Guid("937a7d1d-d10f-4843-80ed-f668ac49cb68")]
