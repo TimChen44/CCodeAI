d133168d20494d8afcc8a07adb546e9ac650606a
TimChen <timchen@live.com>
2023-05-09 13:21:19 +0800

    修正Bug

--- CCodeAI/source.extension.cs
+++ CCodeAI/source.extension.cs
@@ -11,7 +11,7 @@ namespace CCodeAI
         public const string Name = "CCodeAI";
         public const string Description = @"Help you understand and optimize the code";
         public const string Language = "en-US";
-        public const string Version = "2023.509.4";
+        public const string Version = "2023.509.5";
         public const string Author = "TimChen";
         public const string Tags = "";
     }
